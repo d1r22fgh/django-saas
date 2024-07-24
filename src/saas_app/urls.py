c9fb4d1a1041a7b3b95ac35991ab3679818344e9
Darren <d1r22fgh98@gmail.com>
2024-07-24 20:20:18 +0800

    user_only

--- src/saas_app/urls.py
+++ src/saas_app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
 
-from .views import home_view, about_view
+from .views import home_view, about_view, pw_protected_view, user_only_view
 
 urlpatterns = [
     path("", home_view, name='home'),
@@ -26,6 +26,8 @@ urlpatterns = [
     path("register/", auth_views.register_view),
     path("about/", about_view),
     path("hello-world/", home_view),
-    path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('protected/user-only', user_only_view),
+    path('protected/', pw_protected_view),
+    path('admin/', admin.site.urls),
 ]
--- src/saas_app/views.py
+++ src/saas_app/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
 from pathlib import Path
 
 from visits.models import PageVisit
+
+LOGIN_URL = settings.LOGIN_URL
 
 this_dir = Path(__file__).resolve().parent
 
@@ -28,3 +33,24 @@ def about_view(request, *args, **kwargs):
     html_template = 'home.html'
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
+
+VALID_CODE = "abc1234"
+
+def pw_protected_view(request, *args, **kwargs):
+    is_allowed = request.session.get("protected_page_allowed") or 0
+    if request.method == "POST":
+        user_pw_sent = request.POST.get('code') or None
+        if user_pw_sent == VALID_CODE:
+            is_allowed = 1
+            request.session['protected_page_allowed'] = is_allowed
+    if is_allowed:
+        return render(request, "protected/view.html", {})
+    return render(request, "protected/entry.html", {})
+
+@login_required(login_url=LOGIN_URL)
+def user_only_view(request, *args, **kwargs):
+    return render (request, "protected/user_only.html")
+
+@staff_member_required(login_url=LOGIN_URL)
+def user_only_view(request, *args, **kwargs):
+    return render (request, "protected/user_only.html")
